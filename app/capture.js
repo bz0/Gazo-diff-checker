87b1f8ce391f3960f3d3f8618c598af286839c03
bz0 <bzz0217@yahoo.co.jp>
2021-02-28 23:10:30 +0900

    画像遅延ロード対策の為、スクロールさせる

--- app/capture.js
+++ app/capture.js
@@ -1,7 +1,30 @@
 const puppeteer = require('puppeteer');
 
-async function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+async function scrollToBottom(page, viewportHeight) {
+  const getScrollHeight = () => {
+    return Promise.resolve(document.documentElement.scrollHeight) }
+
+  let scrollHeight = await page.evaluate(getScrollHeight)
+  let currentPosition = 0
+  let scrollNumber = 0
+
+  while (currentPosition < scrollHeight) {
+    scrollNumber += 1
+    const nextPosition = scrollNumber * viewportHeight
+    await page.evaluate(function (scrollTo) {
+      return Promise.resolve(window.scrollTo(0, scrollTo))
+    }, nextPosition)
+    await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 5000})
+              .catch(e => console.log('timeout exceed. proceed to next operation'));
+
+    currentPosition = nextPosition;
+    console.log(`scrollNumber: ${scrollNumber}`)
+    console.log(`currentPosition: ${currentPosition}`)
+
+    // 2
+    scrollHeight = await page.evaluate(getScrollHeight)
+    console.log(`ScrollHeight ${scrollHeight}`)
+  }
 }
 
 /**
@@ -16,13 +39,16 @@ async function timeout(ms) {
   // 新しい空のページを開く.
   const page = await browser.newPage();
 
+  const viewportHeight = 1200;
+  const viewportWidth = 1200;
+
   // view portの設定.
   await page.setViewport({
-    width: 1200,
-    height: 800,
+    width: viewportWidth,
+    height: viewportHeight,
   });
   await page.goto('https://www.minnanokaigo.com/');
-  await timeout(5000);
+  await scrollToBottom(page, viewportHeight);
 
   // await page.waitForNavigation({waitUntil:'networkidle2', timeout:5000})
   //            .catch(e => console.log('timeout exceed. proceed to next operation'))
